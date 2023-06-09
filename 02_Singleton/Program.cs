7771e6738f6a496f593c413d17e504ff43e81da8
Toseef Ahmad <Toseef.Ahmad@cireson.com>
2023-06-10 00:16:58 +0500

    Add project files.

---
+++ 02_Singleton/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _02_Singleton;
+
+Console.WriteLine("Sigleton Program");
+
+Singleton FromStudent = Singleton.Instance;
+FromStudent.PrintDetails("From Student");
+
+Singleton FromEmployee = Singleton.Instance;
+FromEmployee.PrintDetails("From Employee");
+
+/*
+ * why we should use sealed keyword in the singleton class
+ * 
+ *Singleton.SingletonDerived singletonDerived = new Singleton.SingletonDerived();
+ *singletonDerived.PrintDetails("Derived Class");
+ *
+ */
+
---
+++ 02_Singleton/Singletion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Singleton
+{
+    public class Singleton
+    {
+        private static int counter = 0;
+        private static Singleton? instance = null;
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
+        }
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter Value: " + counter.ToString());
+        }
+
+        public void PrintDetails(string details)
+        {
+            Console.WriteLine(details);
+        }
+
+        /*
+         * why we should use sealed keyword in the singleton class
+         * 
+         *       public class SingletonDerived : Singleton
+         *       {
+         *       }
+         */
+    }
+}
